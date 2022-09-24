bfadd67abb7d4522d2a5f9b7a35b14298d307880
Κωνσταντινος Χαφης <koxafis@gmail.com>
2022-09-24 17:43:26 +0300

    Lighthouse impovments

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const app = express();
 const portNum = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "100mb" }));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public/static"));
 app.use("/", express.static("public/home", { index: "index.html" }));
